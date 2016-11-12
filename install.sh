3e4f23f9799e8203330791fdcb1105afba35cc06
Benito Sanchez <sanchezbenitog@gmail.com>
2016-11-12 13:03:02 -0800

    Adding a install file

---
+++ install.sh
@@ -0,0 +1,6 @@
+git clone --recursive https://github.com/mihirgogate/dotfiles.git dotfiles
+chmod +x dotfiles/setup.sh
+./dotfiles/setup.sh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"   # install oh-my-zsh
+
+
